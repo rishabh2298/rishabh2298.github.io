c74340a5a75b064102e02ce00d630b856696c3a3
rishabh2298 <rishabhmishra2298@gmail.com>
2022-09-17 04:46:23 +0530

    Home page done with improvement in sidebar

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
-// document.querySelector('#xmark').style.display = "none";
+// Sidebar display section 
 
-document.querySelector("#navControl>i").addEventListener("click", ()=>{
+document.querySelector(".sidebar").style.display = "none";
+document.querySelector("#xmark").style.display = "none";
+
+document.querySelector("#ham").addEventListener("click", ()=>{
     document.querySelector(".sidebar").style.display = "block";
-    document.querySelector("xmark").style.display = "block";
+    document.querySelector("#xmark").style.display = "block";
+    document.querySelector("#ham").style.display = "none";
 });
 
-document.querySelector(".sidebar>i").addEventListener("click", ()=>{
+document.querySelector("#xmark").addEventListener("click", ()=>{
     document.querySelector(".sidebar").style.display = "none";
-    document.querySelector("ham").style.display = "block";
+    document.querySelector("#ham").style.display = "block";
+    document.querySelector("#xmark").style.display = "none";
 })
