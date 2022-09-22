fd07eabe000e3ffaeb6f8913fceb5d856719919f
rishabh2298 <rishabhmishra2298@gmail.com>
2022-09-22 20:39:34 +0530

    Project cards are added (responsive)

--- index.js
+++ index.js
@@ -5,17 +5,37 @@
 
 // Sidebar display section 
 
-document.querySelector(".sidebar").style.display = "none";
-document.querySelector("#xmark").style.display = "none";
+// document.querySelector(".sidebar").style.display = "none";
+// document.querySelector("#xmark").style.display = "none";
 
-document.querySelector("#ham").addEventListener("click", ()=>{
-    document.querySelector(".sidebar").style.display = "block";
-    document.querySelector("#xmark").style.display = "block";
-    document.querySelector("#ham").style.display = "none";
-});
+// document.querySelector("#ham").addEventListener("click", ()=>{
+//     document.querySelector(".sidebar").style.display = "block";
+//     document.querySelector("#xmark").style.display = "block";
+//     document.querySelector("#ham").style.display = "none";
+// });
 
-document.querySelector("#xmark").addEventListener("click", ()=>{
-    document.querySelector(".sidebar").style.display = "none";
-    document.querySelector("#ham").style.display = "block";
-    document.querySelector("#xmark").style.display = "none";
-})
+// document.querySelector("#xmark").addEventListener("click", ()=>{
+//     document.querySelector(".sidebar").style.display = "none";
+//     document.querySelector("#ham").style.display = "block";
+//     document.querySelector("#xmark").style.display = "none";
+// })
+
+
+//   Projects
+
+// let hindu = [];
+// hindu[0] = "hindu1.png"
+// hindu[1] = "hindu2.png"
+
+// let hinduSection = document.querySelector("#hindu");
+
+// let hinduIndex = 0;
+// let hinduPage = setInterval(()=>{
+//     if(hinduIndex==hindu.length){
+//         hinduIndex = 0;
+//     }
+//     let image = document.createElement("img")
+//     image.src = hindu[hinduIndex];
+//     hinduSection.innerHTML = image;
+//     hinduIndex++;
+// },2000)
